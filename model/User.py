b17d7d06abca9609f79d55e7cb2b074fed192f2c
cleytonRR <cleytonrodrigues69@live.com>
2020-07-11 14:35:45 -0300

    Add method to hash password before save

--- model/User.py
+++ model/User.py
@@ -1,6 +1,7 @@
 from sqlalchemy import Column, Integer, String
 from config import dbconfig
 from sqlalchemy.ext.declarative import declarative_base
+from utils.passwordHash import PasswordHash
 
 Base = declarative_base()
 connection = dbconfig.connection()
@@ -12,13 +13,17 @@ class User(Base):
     id = Column(Integer, primary_key=True)
     name = Column(String(40), index=True, nullable=False)
     email = Column(String(80), nullable=False)
-    password = Column(String(40), nullable=False)
+    password = Column(String(128), nullable=False)
 
     def __repr__(self):
         return 'User: {}'.format(self.name)
 
+    def hash_password(self):
+        return PasswordHash.encrypted_password(self.password)
+
     def save(self):
         try:
+            self.password = self.hash_password()
             connection.add(self)
             connection.commit()
         except:
