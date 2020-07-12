70f94782be9b00dab54b734c36bebb2b7f0a2b89
cleytonRR <cleytonrodrigues69@live.com>
2020-07-11 22:41:39 -0300

    Refactor

--- config/dbconfig.py
+++ config/dbconfig.py
@@ -1,7 +1,10 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
+import os
 
-engine = create_engine('postgresql+pg8000://postgres:docker@localhost:5433/chat', convert_unicode=True)
+engine = create_engine('postgresql+pg8000://{}:{}@localhost:5433/chat'.format(os.getenv("DB_USER"),
+                                                                              os.getenv("DB_PASSWORD")),
+                       convert_unicode=True)
 
 def connection():
     return scoped_session(sessionmaker(autocommit=False,
