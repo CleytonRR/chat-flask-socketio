e0d83f0172e0b014dbe6f6aee9432273e68fa17b
cleytonRR <cleytonrodrigues69@live.com>
2020-07-13 15:30:19 -0300

    Added error handling

--- controllers/user_controller.py
+++ controllers/user_controller.py
@@ -1,6 +1,8 @@
 from flask import request
 from flask_restful import Resource
 from model.User import User
+from utils.verify_email import VerifyEmail
+from exceptions.email_exception import EmailException
 
 
 class UserController(Resource):
@@ -8,12 +10,27 @@ class UserController(Resource):
         try:
             dados = request.json
             user = User(name=dados['name'], email=dados['email'], password=dados['password'])
+            if not VerifyEmail.verify(dados['email']):
+                raise EmailException()
             user.save()
             response = {'Name: ': user.name, 'msg': 'Save success'}
+
+        except EmailException:
+            response = {
+                'msg': 'Invalid email'
+            }
+
+            return response, 400
+
+        except KeyError:
+            response = {
+                'msg': 'It is necessary to send username, email and password in json format'
+            }
+            return response, 400
         except:
             response = {
                 'msg': 'Error receiving data'
             }
             return response, 400
-        return response, 201
 
+        return response, 201
